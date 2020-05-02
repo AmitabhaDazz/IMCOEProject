b2d60046e512869184de8f3bb0299f96e16a1d5d
indrasishg <46771631+indrasishg@users.noreply.github.com>
2020-05-02 15:44:13 +0530

    Update FileUpload.java

--- src/main/java/com/imco/costoptimization/app/FileUpload.java
+++ src/main/java/com/imco/costoptimization/app/FileUpload.java
@@ -97,7 +97,6 @@ public class FileUpload extends HttpServlet {
 	
 	private String getFileName(final Part part) {
 	    final String partHeader = part.getHeader("content-disposition");
-//	    LOGGER.log(Level.INFO, "Part Header = {0}", partHeader);
 	    for (String content : part.getHeader("content-disposition").split(";")) {
 	        if (content.trim().startsWith("filename")) {
 	            return content.substring(
