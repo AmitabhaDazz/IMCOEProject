182ca37350457f7cde16143583f0a7d1c5602114
indrasishg <46771631+indrasishg@users.noreply.github.com>
2020-05-02 22:11:50 +0530

    Update FileUpload.java

--- src/main/java/com/imco/costoptimization/app/FileUpload.java
+++ src/main/java/com/imco/costoptimization/app/FileUpload.java
@@ -1,13 +1,17 @@
 package com.imco.costoptimization.app;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -15,6 +19,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
 /**
@@ -70,8 +81,41 @@ public class FileUpload extends HttpServlet {
 	        while ((read = filecontent.read(bytes)) != -1) {
 	            out.write(bytes, 0, read);
 	        }
-	        System.out.println("New file " + fileName + " created at " + path);
-	        writer.println("New file " + fileName + " created at " + path);
+	        
+	        //File myFile = new File(path + File.separator + fileName);
+	        //FileInputStream fis = new FileInputStream(myFile);
+
+	        // Finds the workbook instance for XLSX file
+	        XSSFWorkbook myWorkBook = new XSSFWorkbook (path + File.separator + fileName);
+
+	        // Return first sheet from the XLSX workbook
+	        XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+
+	        // Get iterator to all the rows in current sheet
+	        Iterator<Row> rowIterator = mySheet.iterator();
+	        writer.println("<table >");
+	        while (rowIterator.hasNext()) {
+	            Row row = rowIterator.next();
+	            writer.println("<tr>");
+	            // For each row, iterate through each columns
+	            Iterator<Cell> cellIterator = row.cellIterator();
+	            while (cellIterator.hasNext()) {
+	            	writer.println("<td>");
+		            
+	                Cell cell = cellIterator.next();
+
+	                if (cell.getCellTypeEnum() == CellType.STRING) {
+	                	writer.print(cell.getStringCellValue());
+	                } else if (cell.getCellTypeEnum() == CellType.NUMERIC) {
+	                	writer.print(cell.getNumericCellValue());
+	                }
+	                writer.println("</td>");
+		            
+	            }
+	            writer.println("</tr>"); 
+	        }
+	        writer.println("</table>");
+	        
 //	        LOGGER.log(Level.INFO, "File{0}being uploaded to {1}", 
 //	                new Object[]{fileName, path});
 	    } catch (FileNotFoundException fne) {
@@ -80,6 +124,7 @@ public class FileUpload extends HttpServlet {
 	                + "location.");
 	        writer.println("<br/> ERROR: " + fne.getMessage());
 
+	        
 //	        LOGGER.log(Level.SEVERE, "Problems during file upload. Error: {0}", 
 //	                new Object[]{fne.getMessage()});
 	    } finally {
